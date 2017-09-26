113c435385c6d547b345e3f259d07ddf799c9aca
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2017-09-26 14:32:52 +0200

    Add empty example for compiling minisat decomp solver

---
+++ example/gpid-prop.cpp
@@ -0,0 +1,7 @@
+#define GPID_PROPOSITIONAL_PI_GENERATOR
+
+#include <MinisatPropositionalEngine.hpp>
+
+extern int main(int argc, char** argv) {
+
+}
---
+++ framework/minisat/include/MinisatPropositionalEngine.hpp
@@ -0,0 +1,18 @@
+#ifndef GPID_PROPOSITIONAL_ENGINE__MINISAT_HPP
+#define GPID_PROPOSITIONAL_ENGINE__MINISAT_HPP
+
+#include <minisat/simp/SimpSolver.h>
+#include <DecompEngine.hpp>
+
+namespace gpid_prop {
+
+    typedef Minisat::Lit MinisatHypothesis;
+
+    class MinisatProblem;
+    class MinisatSolver;
+
+    typedef gpid::DecompositionEngine<MinisatHypothesis, MinisatProblem, MinisatSolver> MinisatDecompEngine;
+
+};
+
+#endif
