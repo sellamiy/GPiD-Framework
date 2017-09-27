fb127f8bbf1789fa160534c50c30d5a2881cba21
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2017-09-27 10:43:15 +0200

    Add internal types for Memory lv table

--- framework/gpid/include/Memory.hpp
+++ framework/gpid/include/Memory.hpp
@@ -8,17 +8,26 @@ namespace gpid {
 
         template <typename InternalT>
         class lv_tab {
+            enum ltc_status { INACTIVE, SKIPPED, ACTIVE };
+            struct ltc_elem { InternalT content; ltc_status status; uint32_t level; };
+
+            uint32_t level;
         public:
-            void extend(uint32_t level);
-            void shrink(uint32_t level);
+            void extend(uint32_t lvl);
+            void shrink(uint32_t lvl);
 
-            bool has_next(uint32_t level);
+            bool has_next(uint32_t lvl);
 
-            void set_current(uint32_t level);
-            void unset_current(uint32_t level);
-            void skip_current(uint32_t level);
-            InternalT get_current(uint32_t level);
+            void set_current(uint32_t lvl);
+            void unset_current(uint32_t lvl);
+            void skip_current(uint32_t lvl);
+            InternalT get_current(uint32_t lvl);
         };
+
+        template <typename InternalT>
+        inline void lv_tab<InternalT>::extend(uint32_t lvl) {
+            // TODO
+        }
 
     };
 };
