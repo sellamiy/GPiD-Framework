c665c2ede36af12bd50af9bd06358e89a75e602b
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2017-09-29 14:02:36 +0200

    Clear todos

--- example/gpid-prop.cpp
+++ example/gpid-prop.cpp
@@ -21,9 +21,6 @@ int main(int argc, char** argv) {
 
     MinisatHypothesesSet A(2*P.getVarCpt());
 
-    // TODO: Read problem from file and store clauses in P
-    // TODO: Read problem from file and store abducoble hypotheses in A
-
     MinisatDecompEngine E(S, P, A);
 
     E.generateImplicates();
