98fafaaa5579b56228e98fdf62ff23f94c540b81
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2017-10-02 15:48:15 +0200

    Replace template by basic useable test

--- test/framework/core/HypothesesSet.cpp
+++ test/framework/core/HypothesesSet.cpp
@@ -11,15 +11,17 @@ class HypothesesSetTest : public ::testing::Test {
 protected:
     HypothesesSetTest() { srand(time(NULL)); }
 
-    HypothesesSet<int> set = HypothesesSet<int>(HSET_SIZE);
+    HypothesesSet<int> *set;
 
     virtual void SetUp() {
+        set = new HypothesesSet<int>(HSET_SIZE);
     }
 
     virtual void TearDown() {
+        delete set;
     }
 };
 
-TEST_F(HypothesesSetTest, Example) {
-
+TEST_F(HypothesesSetTest, InitStatus) {
+    ASSERT_FALSE(set->isEmpty(0));
 }
