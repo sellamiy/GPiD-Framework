1904500011f1c06bd544cb75982a2b0c192335f9
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2017-10-02 18:29:40 +0200

    Instanciate a project version header

---
+++ framework/gpid/include/version.hpp
@@ -0,0 +1,19 @@
+#ifndef _PI_DECOMP_VERSION_HEADER_
+#define _PI_DECOMP_VERSION_HEADER_
+
+#include <string>
+
+namespace gpid {
+
+    extern const std::string project_name;
+    extern const std::string project_full_name;
+
+    extern const std::string project_version;
+    extern const std::string project_version_short;
+    extern const std::string project_version_state;
+
+    extern const std::string project_mode;
+
+};
+
+#endif
