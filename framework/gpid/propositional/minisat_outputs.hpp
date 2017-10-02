bd818cf703a3f9e3c190d279c52765baba5059ef
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2017-10-02 14:25:00 +0200

    Complete temp implem of negated implicates printer

--- framework/gpid/propositional/minisat_outputs.hpp
+++ framework/gpid/propositional/minisat_outputs.hpp
@@ -1,11 +1,19 @@
 #ifndef GPID_MINISAT_OUTPUT_HANDLING_HPP
 #define GPID_MINISAT_OUTPUT_HANDLING_HPP
 
+#include <iostream>
+#include <gpid/config.hpp>
 #include <gpid/propositional/minisat_pengine.hpp>
 
+#ifdef MERGE_MINISAT_OUTPUT_WRAPPERS
+namespace Minisat {
+#else
 namespace gpid {
+#endif
 
-    extern void parse_DIMACS(gzFile input_stream, MinisatProblem& P, bool strictp = false);
+    extern inline std::ostream& operator<<(std::ostream& out, const Minisat::Lit p) {
+        return out << (p == Minisat::lit_Undef ? "?" : (p.x % 2 == 0 ? "" : "-")) << p.x/2 + 1;
+    }
 
 };
 
--- framework/src/propositional/minisat_algorithms.cpp
+++ framework/src/propositional/minisat_algorithms.cpp
@@ -1,6 +1,7 @@
 #define MINISAT_GENERATION_INSTANCES
 
 #include <gpid/propositional/minisat_pengine.hpp>
+#include <gpid/propositional/minisat_outputs.hpp>
 #include <gpid/algorithms/bpd_algorithm.hpp>
 
 namespace gpid {
@@ -8,9 +9,8 @@ namespace gpid {
     extern void DecompositionEngine<MinisatHypothesis, MinisatProblem, MinisatSolver>
     ::printAsImplicate(std::vector<MinisatHypothesis>& impl, bool negate) {
         if (negate) {
-            l_warn("Minisat Implicate Negated Printers are not complete yet!");
-            std::cout << "+ ";
-            for (MinisatHypothesis hyp : impl) std::cout /* << ~(hip.lit) */ << " ";
+            std::cout << "> ";
+            for (MinisatHypothesis hyp : impl) std::cout << ~(hyp.lit) << " ";
             std::cout << std::endl;
         } else {
             l_warn("Minisat Implicate Non-negated Printers are not implemeted yet!");
