f352e136718cc5aa49167bccfb1fe466dce2e17f
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2017-10-06 15:26:54 +0200

    Improve generability of snlog printers

--- framework/gpid/propositional/minisat_outputs.hpp
+++ framework/gpid/propositional/minisat_outputs.hpp
@@ -17,6 +17,22 @@ namespace gpid {
         return out << (p == Minisat::lit_Undef ? "?" : (p.x % 2 == 0 ? "" : "-")) << p.x/2 + 1;
     }
 
+    extern inline std::ostream& operator<<(std::ostream& out, const Minisat::lbool p) {
+        return out << (p == Minisat::l_Undef ? "U" : p == Minisat::l_True ? "T" : "F");
+    }
+
+    extern inline std::ostream& operator<<(std::ostream& out, const Minisat::vec<Minisat::lbool>& m) {
+        out << "< ";
+        for (int i = 0; i < m.size(); ++i) out << m[i] << " ";
+        return out << ">";
+    }
+
+    extern inline std::ostream& operator<<(std::ostream& out, const Minisat::vec<Minisat::Lit>& c) {
+        out << "< ";
+        for (int i = 0; i < c.size(); ++i) out << c[i] << " ";
+        return out << ">";
+    }
+
     extern inline
     void p_implicate(std::ostream& out, Minisat::vec<Minisat::Lit>& impl, bool negate) {
         out << "> ";
@@ -28,6 +44,12 @@ namespace gpid {
 
 /* Gpid Minisat integr. output printers */
 namespace gpid {
+
+    extern inline std::ostream& operator<<(std::ostream& out, const std::vector<MinisatHypothesis>& c) {
+        out << "< ";
+        for (MinisatHypothesis hyp : c) out << hyp.lit << " ";
+        return out << ">";
+    }
 
     extern inline
     void p_implicate(std::ostream& out, std::vector<MinisatHypothesis>& impl, bool negate) {
