87bf0a4d7daf030bcb984950934a9e9b0afa5f3c
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2017-10-06 18:05:54 +0200

    Log on example executable

--- example/gpid-prop.cpp
+++ example/gpid-prop.cpp
@@ -4,12 +4,15 @@
 #include <gpid/gpid.hpp>
 
 using namespace std;
+using namespace snlog;
 using namespace Minisat;
 using namespace gpid;
 
 int main(int argc, char** argv) {
+    l_message("start propositional implicate generator...");
     if (argc != 2) {
-        cout << "usage: gpid-prop <input-file>" << endl;
+        l_fatal("usage: gpid-prop <input-file>");
+        return EXIT_FAILURE;
     }
 
     CoreOptions O;
@@ -17,15 +20,20 @@ int main(int argc, char** argv) {
     MinisatSolver S;
     MinisatProblem P;
 
+    l_message("parse problem...");
     gzFile in = gzopen(argv[1], "rb"); // TODO: Handle input errors
     parse_DIMACS(in, P);
     gzclose(in);
 
+    l_message("generate decompostition structures...");
     MinisatHypothesesSet A(2*P.getVarCpt());
     initRawSet(A);
 
     MinisatDecompEngine E(O.engine, S, P, A);
 
+    l_message("generate implicates...");
     E.generateImplicates();
 
+    l_message("complete.");
+    return EXIT_SUCCESS;
 }
