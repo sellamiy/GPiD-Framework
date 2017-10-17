9b743a36c8e06863e917350c02cf16c21fa87ee4
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2017-10-17 14:00:44 +0200

    Add a strict abducible parser executable
    Allows for testing the parser

---
+++ example/gpid-parser.cpp
@@ -0,0 +1,40 @@
+#define GPID_GTS_PI_GENERATOR
+
+#include <iostream>
+#include <gpid/gpid.hpp>
+
+#include "utils/parser-options.hpp"
+
+using namespace std;
+using namespace snlog;
+using namespace gpid;
+
+int main(int argc, char** argv) {
+    ParserOptions opts;
+    OptionStatus status = parseOptions(opts, argc, argv);
+    if (status == OptionStatus::FAILURE) {
+	return EXIT_FAILURE;
+    } else if (status == OptionStatus::ENDED) {
+	return EXIT_SUCCESS;
+    }
+
+    l_message("start abducible parser...");
+
+    AbducibleParser parser(opts.input);
+    parser.init();
+    t_fatal(!parser.isOk(), "Parser in broken state; please stop!");
+
+    l_notif("number of abducibles", parser.getAbducibleCount());
+
+    for (uint32_t i = 0; i < parser.getAbducibleCount(); i++) {
+        l_notif("abducible", parser.nextAbducible());
+    }
+
+    if (parser.isOk()) {
+        l_message("complete.");
+        return EXIT_SUCCESS;
+    } else {
+        l_fatal("errors were raised.");
+        return EXIT_FAILURE;
+    }
+}
---
+++ example/utils/parser-options.hpp
@@ -0,0 +1,77 @@
+#ifndef _INCL_EX_OPTIONS_PARSER_
+#define _INCL_EX_OPTIONS_PARSER_
+
+#include <vector>
+#include <cxxopts/cxxopts.hpp>
+#include <gpid/gpid.hpp>
+
+/* ===== Structures ===== */
+
+struct ParserOptions {
+    std::string input;
+};
+
+enum OptionStatus {
+    OK, ENDED, FAILURE
+};
+
+static inline OptionStatus parseOptions(ParserOptions& opts, int argc, char** argv);
+static inline OptionStatus handleOptions(ParserOptions& opts, cxxopts::Options& parser);
+
+/* ===== Parser ===== */
+
+static inline OptionStatus parseOptions(ParserOptions& opts, int argc, char** argv) {
+    try {
+
+	cxxopts::Options parser(argv[0], gpid::project_full_name + "--parser");
+
+	parser.add_options()
+	    ("h,help", "Print this help message")
+	    ("v,version", "Print framework version")
+	    ;
+
+	parser.add_options("Input")
+	    ("i,input", "Input file", cxxopts::value<std::string>())
+	    ;
+
+	parser.parse(argc, argv);
+
+	return handleOptions(opts, parser);
+
+    } catch (const cxxopts::OptionException& e) {
+	snlog::l_error(e.what());
+	return OptionStatus::FAILURE;
+    }
+}
+
+/* ===== Handler ===== */
+
+static inline OptionStatus handleOptions(ParserOptions& opts, cxxopts::Options& parser) {
+    try {
+
+	if (parser.count("help")) {
+	    snlog::l_message(parser.help({"", "Input"}));
+	    return OptionStatus::ENDED;
+	}
+	if (parser.count("version")) {
+	    snlog::l_message(gpid::generate_version_message());
+	    return OptionStatus::ENDED;
+	}
+
+	if (parser.count("input")) {
+	    opts.input = parser["input"].as<std::string>();
+	} else {
+	    snlog::l_fatal("No input file provided");
+	    snlog::l_info(parser.help({"Input"}));
+	    return OptionStatus::FAILURE;
+	}
+
+	return OptionStatus::OK;
+
+    } catch (const cxxopts::OptionException& e) {
+	snlog::l_error(e.what());
+	return OptionStatus::FAILURE;
+    }
+}
+
+#endif
