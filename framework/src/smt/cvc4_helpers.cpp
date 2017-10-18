eade5f981f4a42c2f8d109e271fdf849d9e77484
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2017-10-18 11:11:26 +0200

    Cleanup helpers

--- framework/src/smt/cvc4_helpers.cpp
+++ framework/src/smt/cvc4_helpers.cpp
@@ -7,25 +7,6 @@
 using namespace snlog;
 using namespace CVC4;
 
-static const std::string c4helpers_gab_tag = "CVC4Hypotheses";
-/*
-extern void gpid::initRawSet(ExprManager& em, CVC4HypothesesSet& set) {
-    GAB_Status res;
-    res = requestContinuousArray(c4helpers_gab_tag, set.getSourceSize(), sizeof(CVC4Hypothesis));
-    if (res != GAB_Status::SUCCESS) l_error("Memory request for minisat hypotheses wrappers failed!");
-    for (uint32_t i = 0; i < set.getSourceSize(); i++) {
-        Expr cstl = em.mkConst(false);
-        l_warn("Fixme: add correct abducible instanciation for cvc4 expression instances"); // TODO
-        CVC4Hypothesis *mloc;
-        res = accessContinuousPointer(c4helpers_gab_tag, i, (void**)&mloc);
-        if (res != GAB_Status::SUCCESS) l_error("Memory access for minisat hypothesis wrapper failed!");
-        new (mloc) CVC4Hypothesis(cstl);
-        set.mapHypothesis(i, mloc);
-        // set.mapLink(i, i%2 == 0 ? i+1 : i-1); // TODO Linkage
-        l_warn("Fixme: possibly, add abducible linkage for cvc4 expression instances"); // TODO
-    }
-}
-*/
 namespace gpid {
 
     enum c4InputGenerator { C4IG_NONE };
--- framework/src/smt/z3_helpers.cpp
+++ framework/src/smt/z3_helpers.cpp
@@ -7,25 +7,6 @@
 using namespace snlog;
 using namespace z3;
 
-static const std::string z3helpers_gab_tag = "Z3Hypotheses";
-/*
-extern void gpid::initRawSet(context& ctx, Z3HypothesesSet& set) {
-    GAB_Status res;
-    res = requestContinuousArray(z3helpers_gab_tag, set.getSourceSize(), sizeof(Z3Hypothesis));
-    t_error(res != GAB_Status::SUCCESS, "Memory request for minisat hypotheses wrappers failed!");
-    for (uint32_t i = 0; i < set.getSourceSize(); i++) {
-        expr cstl = ctx.bool_val(false);
-        l_warn("Fixme: add correct abducible instanciation for z3 expression instances"); // TODO
-        Z3Hypothesis *mloc;
-        res = accessContinuousPointer(z3helpers_gab_tag, i, (void**)&mloc);
-        t_error(res != GAB_Status::SUCCESS, "Memory access for minisat hypothesis wrapper failed!");
-        new (mloc) Z3Hypothesis(cstl);
-        set.mapHypothesis(i, mloc);
-        // set.mapLink(i, i%2 == 0 ? i+1 : i-1); // TODO Linkage
-        l_warn("Fixme: possibly, add abducible linkage for z3 expression instances"); // TODO
-    }
-}
-*/
 namespace gpid {
 
     enum z3InputGenerator { Z3IG_NONE };
