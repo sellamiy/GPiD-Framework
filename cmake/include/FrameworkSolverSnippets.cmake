c7a687903d6870382d350a744cbbd9d0be80b1ea
Yanis Sellami <yanisellami@gmail.com>
2017-11-19 11:36:18 +0100

    Generate single solver snippets independently

--- cmake/include/FrameworkSolverSnippets.cmake
+++ cmake/include/FrameworkSolverSnippets.cmake
@@ -16,26 +16,30 @@ macro(generate_solver_snippet target_directory config_directory snippet_name)
   endforeach()
 
   if(EXISTS "${SNIPPET_AS_SINGLE_PATH}")
-    list(APPEND CURRENT_SNIPPET_DEPENDS_TARGETS ${SNIPPET_AS_SINGLE_PATH})
     set(CURRENT_SNIPPET_OUTPUT_TARGETS "")
     foreach(solver_name ${ARGN})
       list(APPEND CURRENT_SNIPPET_OUTPUT_TARGETS
 	"${target_directory}/${solver_name}_${snippet_name}")
+      add_custom_command(
+	OUTPUT "${target_directory}/${solver_name}_${snippet_name}"
+	DEPENDS ${SNIPPET_AS_SINGLE_PATH}
+	"${config_directory}/${solver_name}.yaml"
+	COMMAND ${RUBY_EXECUTABLE} ${SNIPPETS_GENERATOR}
+	${target_directory} ${config_directory} ${snippet_name} ${solver_name})
     endforeach()
 
   elseif(EXISTS "${SNIPPET_AS_MULTI_PATH}")
     list(APPEND CURRENT_SNIPPET_DEPENDS_TARGETS ${SNIPPET_AS_MULTI_PATH})
     set(CURRENT_SNIPPET_OUTPUT_TARGETS
       "${target_directory}/${snippet_name}")
+    add_custom_command(
+      OUTPUT "${target_directory}/${snippet_name}"
+      DEPENDS ${CURRENT_SNIPPET_DEPENDS_TARGETS}
+      COMMAND ${RUBY_EXECUTABLE} ${SNIPPETS_GENERATOR} ${ARGV})
 
   else()
     message(ERROR "Unknown solver snippet: ${snippet_name}")
   endif()
 
-  if(CURRENT_SNIPPET_OUTPUT_TARGETS)
-    add_custom_command(OUTPUT ${CURRENT_SNIPPET_OUTPUT_TARGETS}
-      DEPENDS ${CURRENT_SNIPPET_DEPENDS_TARGETS}
-      COMMAND ${RUBY_EXECUTABLE} ${SNIPPETS_GENERATOR} ${ARGV})
-  endif()
 endmacro()
 # ==============================================================================
