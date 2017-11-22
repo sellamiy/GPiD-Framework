9e4c8062740903ba5fbba553624716878e8f0704
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2017-11-22 16:44:35 +0100

    Generate CVC4 solver interface from snippet w/ rgentor

--- cmake/include/FrameworkSolverRegistration.cmake
+++ cmake/include/FrameworkSolverRegistration.cmake
@@ -3,22 +3,60 @@
 # ==============================================================================
 include(FrameworkSolverSnippets)
 # ==============================================================================
-macro(register_solver_library solver_name solver_includes)
+macro(generate_solver_interface_sources solver_name solver_snippets_dir)
+  file(GLOB SNIPPETS_SOURCES
+    "${solver_snippets_dir}/*.hpp"
+    "${solver_snippets_dir}/*.cpp"
+    "${solver_snippets_dir}/*.spp")
+  set(INTERFACE_HEADERS_TARGET gpid-${solver_name}-headers)
+  set(INTERFACE_SOURCES_TARGET gpid-${solver_name}-sources)
+
+  foreach(local_snippet ${SNIPPETS_SOURCES})
+    get_filename_component(local_snippet_name ${local_snippet} NAME)
+    configure_file(${local_snippet}
+      "${LOCAL_SOLVERS_SNIPPETS_DIR}/${solver_name}/${local_snippet_name}" COPYONLY)
+  endforeach()
+
+  set(REQUIRED_HEADER_SNIPPETS
+    context.hpp inputs.hpp outputs.hpp problem.hpp engine.hpp solver.hpp)
+  set(REQUIRED_SOURCE_SNIPPETS
+    algorithms.cpp context.cpp problem.cpp solver.cpp parsers.cpp abducible_wrappers.cpp)
+
+  foreach(snippet ${REQUIRED_HEADER_SNIPPETS})
+    generate_solver_snippet(${LOCAL_SOLVERS_INTERFACES_HEADER_DIR}
+      ${LOCAL_SOLVERS_SNIPPETS_CONFIG} ${snippet} ${solver_name})
+    list(APPEND INTERFACE_HEADERS "${CURRENT_SNIPPET_OUTPUT_TARGETS}")
+  endforeach()
+  add_custom_target(${INTERFACE_HEADERS_TARGET} DEPENDS ${INTERFACE_HEADERS})
+
+  foreach(snippet ${REQUIRED_SOURCE_SNIPPETS})
+    generate_solver_snippet(${LOCAL_SOLVERS_INTERFACES_SOURCE_DIR}
+      ${LOCAL_SOLVERS_SNIPPETS_CONFIG} ${snippet} ${solver_name})
+    list(APPEND INTERFACE_SOURCES "${CURRENT_SNIPPET_OUTPUT_TARGETS}")
+  endforeach()
+  add_custom_target(${INTERFACE_SOURCES_TARGET} DEPENDS ${INTERFACE_SOURCES})
+endmacro()
+# ==============================================================================
+macro(register_solver_library solver_name solver_snippets_dir)
   # ====== Define names ======
-  set(INTERFACE_SOURCES ${ARGN})
+  set(INTERFACE_SOURCES "")
   set(INTERFACE_TARGET gpid-${solver_name})
   set(INTERFACE_STATIC_TARGET gpid-${solver_name}-static)
   set(INTERFACE_SHARED_TARGET gpid-${solver_name}-shared)
+  # ====== Define sources targets ======
+  generate_solver_interface_sources(${solver_name} ${solver_snippets_dir})
   # ====== Define libraries targets ======
   add_library(${INTERFACE_STATIC_TARGET} STATIC ${INTERFACE_SOURCES})
   add_library(${INTERFACE_SHARED_TARGET} SHARED ${INTERFACE_SOURCES})
+  add_dependencies(${INTERFACE_STATIC_TARGET} ${INTERFACE_HEADERS_TARGET})
+  add_dependencies(${INTERFACE_SHARED_TARGET} ${INTERFACE_HEADERS_TARGET})
   target_include_directories(${INTERFACE_STATIC_TARGET}
-    PUBLIC "${solver_includes}"
+    PUBLIC
     "${LOCAL_SOLVERS_HEADERS_INCLUDE}"
     "${LOCAL_SOLVERS_SNIPPETS_INCLUDE}"
     "${LOCAL_SOLVERS_INTERFACES_INCLUDE}")
   target_include_directories(${INTERFACE_SHARED_TARGET}
-    PUBLIC "${solver_includes}"
+    PUBLIC
     "${LOCAL_SOLVERS_HEADERS_INCLUDE}"
     "${LOCAL_SOLVERS_SNIPPETS_INCLUDE}"
     "${LOCAL_SOLVERS_INTERFACES_INCLUDE}")
@@ -35,17 +73,17 @@ macro(register_solver_library solver_name solver_includes)
   # ====== Add solver interface header target ======
   generate_solver_snippet(${LOCAL_SOLVERS_HEADERS_DIR}
     ${LOCAL_SOLVERS_SNIPPETS_CONFIG} interface.hpp ${solver_name})
-  set(INTERFACE_HEADERS ${CURRENT_SNIPPET_OUTPUT_TARGETS})
-  set(INTERFACE_HEADERS_TARGET gpid-solvers-headers-${solver_name})
-  add_custom_target(${INTERFACE_HEADERS_TARGET} DEPENDS ${INTERFACE_HEADERS})
-  add_dependencies(${INTERFACE_STATIC_TARGET} ${INTERFACE_HEADERS_TARGET})
-  add_dependencies(${INTERFACE_SHARED_TARGET} ${INTERFACE_HEADERS_TARGET})
+  set(INTERFACE_MAIN_HEADER ${CURRENT_SNIPPET_OUTPUT_TARGETS})
+  set(INTERFACE_MAIN_HEADER_TARGET gpid-solvers-headers-${solver_name})
+  add_custom_target(${INTERFACE_MAIN_HEADER_TARGET} DEPENDS ${INTERFACE_MAIN_HEADER})
+  add_dependencies(${INTERFACE_STATIC_TARGET} ${INTERFACE_MAIN_HEADER_TARGET})
+  add_dependencies(${INTERFACE_SHARED_TARGET} ${INTERFACE_MAIN_HEADER_TARGET})
   # ====== Update solvers lists ======
   set(SOLVERS_TARGETS ${SOLVERS_TARGETS} ${solver_name} PARENT_SCOPE)
   set(SOLVERS_STATIC_TARGETS ${SOLVERS_STATIC_TARGETS} ${INTERFACE_STATIC_TARGET} PARENT_SCOPE)
   set(SOLVERS_SHARED_TARGETS ${SOLVERS_SHARED_TARGETS} ${INTERFACE_SHARED_TARGET} PARENT_SCOPE)
-  set(SOLVERS_INTERFACE_TARGETS ${SOLVERS_INTERFACE_TARGETS} ${INTERFACE_HEADERS_TARGET})
-  set(SOLVERS_INTERFACE_HEADERS ${SOLVERS_INTERFACE_HEADERS} ${INTERFACE_HEADERS})
+  set(SOLVERS_INTERFACE_TARGETS ${SOLVERS_INTERFACE_TARGETS} ${INTERFACE_MAIN_HEADER_TARGET})
+  set(SOLVERS_INTERFACE_HEADERS ${SOLVERS_INTERFACE_HEADERS} ${INTERFACE_MAIN_HEADER})
 endmacro()
 # ==============================================================================
 macro(target_solver_static_libraries solver_name)
--- solvers/cvc4/cxx/src/cvc4_algorithms.cpp
+++
@@ -1,9 +0,0 @@
-#define CVC4_GENERATION_INSTANCES
-
-#include <gpid/solvers-wrap/cvc4_engine.hpp>
-#include <gpid/solvers-wrap/cvc4_solver.hpp>
-#include <gpid/algorithms/pid_algorithm.hpp>
-
-namespace gpid {
-    template class DecompositionEngine<CVC4Solver>;
-};
