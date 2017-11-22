713425d0dcbae8635fafde2f58e768c38954d922
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2017-11-22 13:48:05 +0100

    Generate single solvers interface headers as part of registration
    This is cleaner as it is solver-dependent and avoid the necessity to reset
    solver-specific name variables in the general solvers cmake script

--- cmake/include/FrameworkSolverRegistration.cmake
+++ cmake/include/FrameworkSolverRegistration.cmake
@@ -1,11 +1,15 @@
 # ==============================================================================
 # Solver registration utilities
 # ==============================================================================
+include(FrameworkSolverSnippets)
+# ==============================================================================
 macro(register_solver_library solver_name solver_includes)
+  # ====== Define names ======
   set(INTERFACE_SOURCES ${ARGN})
   set(INTERFACE_TARGET gpid-${solver_name})
   set(INTERFACE_STATIC_TARGET gpid-${solver_name}-static)
   set(INTERFACE_SHARED_TARGET gpid-${solver_name}-shared)
+  # ====== Define libraries targets ======
   add_library(${INTERFACE_STATIC_TARGET} STATIC ${INTERFACE_SOURCES})
   add_library(${INTERFACE_SHARED_TARGET} SHARED ${INTERFACE_SOURCES})
   target_include_directories(${INTERFACE_STATIC_TARGET}
@@ -25,11 +29,23 @@ macro(register_solver_library solver_name solver_includes)
     OUTPUT_NAME ${INTERFACE_TARGET}
     VERSION ${VERSION}
     CLEAN_DIRECT_OUTPUT 1)
+  # ====== Link to the core library ======
   target_link_libraries(${INTERFACE_STATIC_TARGET} gpid-core-static)
   target_link_libraries(${INTERFACE_SHARED_TARGET} gpid-core-shared)
+  # ====== Add solver interface header target ======
+  generate_solver_snippet(${LOCAL_SOLVERS_HEADERS_DIR}
+    ${LOCAL_SOLVERS_SNIPPETS_CONFIG} interface.hpp ${solver_name})
+  set(INTERFACE_HEADERS ${CURRENT_SNIPPET_OUTPUT_TARGETS})
+  set(INTERFACE_HEADERS_TARGET gpid-solvers-headers-${solver_name})
+  add_custom_target(${INTERFACE_HEADERS_TARGET} DEPENDS ${INTERFACE_HEADERS})
+  add_dependencies(${INTERFACE_STATIC_TARGET} ${INTERFACE_HEADERS_TARGET})
+  add_dependencies(${INTERFACE_SHARED_TARGET} ${INTERFACE_HEADERS_TARGET})
+  # ====== Update solvers lists ======
   set(SOLVERS_TARGETS ${SOLVERS_TARGETS} ${solver_name} PARENT_SCOPE)
   set(SOLVERS_STATIC_TARGETS ${SOLVERS_STATIC_TARGETS} ${INTERFACE_STATIC_TARGET} PARENT_SCOPE)
   set(SOLVERS_SHARED_TARGETS ${SOLVERS_SHARED_TARGETS} ${INTERFACE_SHARED_TARGET} PARENT_SCOPE)
+  set(SOLVERS_INTERFACE_TARGETS ${SOLVERS_INTERFACE_TARGETS} ${INTERFACE_HEADERS_TARGET})
+  set(SOLVERS_INTERFACE_HEADERS ${SOLVERS_INTERFACE_HEADERS} ${INTERFACE_HEADERS})
 endmacro()
 # ==============================================================================
 macro(target_solver_static_libraries solver_name)
