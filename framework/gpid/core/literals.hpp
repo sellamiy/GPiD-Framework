2cd8266baf40680d54e9bf8a3bcd7d3c283cdac2
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2018-05-24 14:06:13 +0200

    Hypothesis should be wrapped to be used for storage algs

--- framework/gpid/core/literals.hpp
+++ framework/gpid/core/literals.hpp
@@ -19,37 +19,6 @@
 
 namespace gpid {
 
-    template<class LiteralT>
-    class LiteralHypothesis {
-    public:
-        typedef uint32_t index_t;
-    private:
-        starray::SequentialActivableArray _array;
-        std::map<uint32_t, std::vector<index_t>> _lmapping;
-    public:
-        LiteralHypothesis(uint32_t size) : _array(size) {
-            for (uint32_t i = 0; i < size; ++i) _array.deactivate(i);
-        }
-
-        inline void addLiteral(index_t lidx, uint32_t lkey) {
-            _array.activate(lidx);
-            _lmapping[lkey].push_back(lidx);
-        }
-        inline void removeLiterals(uint32_t lkey) {
-            for (index_t lidx : _lmapping[lkey]) {
-                _array.deactivate(lidx);
-            }
-            _lmapping[lkey].clear();
-        }
-
-        typedef typename starray::SequentialActivableArray::iterator iterator;
-        typedef typename starray::SequentialActivableArray::const_iterator const_iterator;
-        inline iterator begin() { return _array.begin(); }
-        inline iterator end() { return _array.end(); }
-        inline const_iterator cbegin() { return _array.cbegin(); }
-        inline const_iterator cend() { return _array.cend(); }
-    };
-
     /** \brief Class for deciding on skipping literals. */
     template<class SolverT>
     class LiteralSkipper {
--- framework/gpid/core/wrappers.hpp
+++ framework/gpid/core/wrappers.hpp
@@ -8,6 +8,7 @@
 #define GPID_FRAMEWORK__CORE__WRAPPERS_HPP
 
 #include <map>
+#include <starray/starray.hpp>
 
 namespace gpid {
 
@@ -19,6 +20,37 @@ namespace gpid {
         inline LiteralT& get(index_t idx) { return *_mapping[idx]; }
     private:
         std::map<index_t, LiteralT*> _mapping;
+    };
+
+    template<class LiteralT>
+    class LiteralHypothesis {
+    public:
+        typedef uint32_t index_t;
+    private:
+        starray::SequentialActivableArray _array;
+        std::map<uint32_t, std::vector<index_t>> _lmapping;
+    public:
+        LiteralHypothesis(uint32_t size) : _array(size) {
+            for (uint32_t i = 0; i < size; ++i) _array.deactivate(i);
+        }
+
+        inline void addLiteral(index_t lidx, uint32_t lkey) {
+            _array.activate(lidx);
+            _lmapping[lkey].push_back(lidx);
+        }
+        inline void removeLiterals(uint32_t lkey) {
+            for (index_t lidx : _lmapping[lkey]) {
+                _array.deactivate(lidx);
+            }
+            _lmapping[lkey].clear();
+        }
+
+        typedef typename starray::SequentialActivableArray::iterator iterator;
+        typedef typename starray::SequentialActivableArray::const_iterator const_iterator;
+        inline iterator begin() { return _array.begin(); }
+        inline iterator end() { return _array.end(); }
+        inline const_iterator cbegin() { return _array.cbegin(); }
+        inline const_iterator cend() { return _array.cend(); }
     };
 
 }
