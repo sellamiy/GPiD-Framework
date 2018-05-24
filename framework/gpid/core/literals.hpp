0f7fee000925aa2ef4b4f508cb97dcfc1eb4d30a
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2018-05-24 13:58:21 +0200

    Add a local hypothesis storer for keeping literals ordered

--- framework/gpid/core/literals.hpp
+++ framework/gpid/core/literals.hpp
@@ -19,6 +19,37 @@
 
 namespace gpid {
 
+    template<class LiteralT>
+    class LiteralHypothesis {
+    public:
+        typedef uint32_t index_t;
+    private:
+        starray::SequentialActivableArray _array;
+        std::map<uint32_t, std::vector<index_t>> _lmapping;
+    public:
+        LiteralHypothesis(uint32_t size) : _array(size) {
+            for (uint32_t i = 0; i < size; ++i) _array.deactivate(i);
+        }
+
+        inline void addLiteral(index_t lidx, uint32_t lkey) {
+            _array.activate(lidx);
+            _lmapping[lkey].push_back(lidx);
+        }
+        inline void removeLiterals(uint32_t lkey) {
+            for (index_t lidx : _lmapping[lkey]) {
+                _array.deactivate(lidx);
+            }
+            _lmapping[lkey].clear();
+        }
+
+        typedef typename starray::SequentialActivableArray::iterator iterator;
+        typedef typename starray::SequentialActivableArray::const_iterator const_iterator;
+        inline iterator begin() { return _array.begin(); }
+        inline iterator end() { return _array.end(); }
+        inline const_iterator cbegin() { return _array.cbegin(); }
+        inline const_iterator cend() { return _array.cend(); }
+    };
+
     /** \brief Class for deciding on skipping literals. */
     template<class SolverT>
     class LiteralSkipper {
@@ -55,6 +86,7 @@ namespace gpid {
     private:
         SolverT& solver;
         AbducibleTree<SolverT> storage;
+        LiteralHypothesis<LiteralT> hypothesis;
 
         typedef uint32_t index_t;
         typedef uint32_t level_t;
@@ -88,7 +120,7 @@ namespace gpid {
         inline index_t getCurrentIndex();
     public:
         LiteralsEngine(SolverT& solver, SkipperController& ctrler, uint32_t size)
-            : solver(solver), storage(solver), l_active(size),
+            : solver(solver), storage(solver), hypothesis(size), l_active(size),
               skipper(solver, storage, l_mapper, ctrler), clevel(1)
         { limit[1] = 0; pointer[1] = size; }
         /** Map an index of the set to a specific literal. */
@@ -236,10 +268,12 @@ namespace gpid {
         }
         deactivateSequents(selected, clevel);
         solver.addLiteral(getCurrentLiteral(), clevel);
+        hypothesis.addLiteral(getCurrentIndex(), clevel);
     }
 
     template<class SolverT>
     inline void LiteralsEngine<SolverT>::unselectLevel(uint32_t level) {
+        hypothesis.removeLiterals(level);
         solver.removeLiterals(level);
         for (index_t skipped : selection_map[level]) {
             if (l_active.is_paused(skipped)) {
