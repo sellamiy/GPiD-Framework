04f5639b5e26a4f89c8d2dfdc06f3379972d9e45
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2018-06-01 10:19:59 +0200

    Show error on c:system calls failure

--- lib/dot/src/System.cpp
+++ lib/dot/src/System.cpp
@@ -1,16 +1,17 @@
 #define LIB_DOT__SYSTEM_CPP
 #include <cstdlib>
 #include <fstream>
+#include <snlog/snlog.hpp>
 #include <dot/dotcommand.hpp>
 
 #ifdef DOT_FOUND
 extern void dot::system::autocompile(std::string source, std::string target) {
     std::string command = DOT_EXEC_PATH " -Tsvg -o ";
     command += target + " " + source;
-    std::system(command.c_str());
+    int ret = std::system(command.c_str());
+    snlog::t_error(ret != 0, "Dot autocompilation returned non 0");
 }
 #else
-#include <snlog/snlog.hpp>
 extern void dot::system::autocompile(std::string, std::string) {
     snlog::l_internal("Dot autocompilation not configured");
 }
