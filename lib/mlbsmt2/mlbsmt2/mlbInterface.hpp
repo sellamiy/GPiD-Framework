64a89e4669a121518666f7da57f83243ca9299dc
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2018-09-26 14:12:55 +0200

    Add interface for in-c++ smtlib2 (abd) literal generator

---
+++ lib/mlbsmt2/mlbsmt2/mlbInterface.hpp
@@ -0,0 +1,109 @@
+#ifndef MAGIC_LITERAL_BUILDER_f_SMTLIB2__INTERFACE_HPP
+#define MAGIC_LITERAL_BUILDER_f_SMTLIB2__INTERFACE_HPP
+
+#include <list>
+#include <map>
+#include <string>
+#include <memory>
+
+namespace mlbsmt2 {
+
+    enum class DataExploitation {
+
+    };
+
+    class MagicProductionRule {
+        std::list<DataExploitation> requirements;
+    public:
+
+        struct ProductionData {
+            const std::string value;
+            bool valid;
+            ProductionData(const std::string value, bool valid=true)
+                : value(value), valid(valid) {}
+        };
+
+        MagicProductionRule& requires(DataExploitation e);
+        inline const std::list<DataExploitation>& getRequirements() const
+        { return requirements; }
+
+        virtual bool hasNext() = 0;
+        virtual ProductionData& next() = 0;
+    };
+
+    using MagicProductionRulePtr = std::shared_ptr<MagicProductionRule>;
+
+    class MagicLiteralData;
+
+    class MagicLiteralBuilder {
+        enum class BuilderState { Initialized, Exploited, Building, Complete, Error };
+        BuilderState state;
+
+        std::list<MagicProductionRulePtr> rules;
+        std::map<DataExploitation, bool> exploitations;
+
+        std::unique_ptr<MagicLiteralData> data;
+
+        bool exploitData(DataExploitation e);
+    public:
+        MagicLiteralBuilder();
+
+        MagicLiteralBuilder& uses(MagicProductionRulePtr& rule);
+
+        void loadSMTlib2File(const std::string filename);
+
+        void exploitData();
+
+        inline bool valid() { return state != BuilderState::Error; }
+        inline bool complete() { return state == BuilderState::Complete; }
+        inline bool exploitable() { return state == BuilderState::Initialized; }
+        inline bool buildable() {
+            return state == BuilderState::Exploited
+                || state == BuilderState::Building;
+        }
+
+        std::string buildLiteral();
+    };
+
+    // Implementations
+
+    inline MagicProductionRule& MagicProductionRule::requires(DataExploitation e) {
+        requirements.push_back(e);
+        return *this;
+    }
+
+    inline MagicLiteralBuilder& MagicLiteralBuilder::uses(MagicProductionRulePtr& rule) {
+        rules.push_back(rule);
+        return *this;
+    }
+
+    inline void MagicLiteralBuilder::exploitData() {
+        if (!exploitable()) { /* TODO: Raise Error */ }
+        for (auto rule : rules) {
+            for (auto exploitation : rule->getRequirements()) {
+                if (!exploitations[exploitation])
+                    exploitations[exploitation] = exploitData(exploitation);
+                if (!exploitations[exploitation])
+                    state = BuilderState::Error;
+            }
+        }
+        state = BuilderState::Exploited;
+    }
+
+    inline std::string MagicLiteralBuilder::buildLiteral() {
+        if (!buildable()) { /* TODO: Raise Error */ }
+        if (state == BuilderState::Exploited)
+            state = BuilderState::Building;
+        typename MagicProductionRule::ProductionData& data = rules.front()->next();
+        if (!rules.front()->hasNext())
+            rules.pop_front();
+        if (rules.empty())
+            state = BuilderState::Complete;
+        if (!data.valid)
+            state = BuilderState::Error;
+        return data.value;
+    }
+
+}
+
+#endif
---
+++ lib/mlbsmt2/src/mlbInterface.cpp
@@ -0,0 +1,26 @@
+#define MAGIC_LITERAL_BUILDER_f_SMTLIB2__INTERFACE_CPP
+
+#include <snlog/snlog.hpp>
+#include <mlbsmt2/mlbInterface.hpp>
+
+namespace mlbsmt2 {
+
+    class MagicLiteralData {
+
+    };
+
+    MagicLiteralBuilder::MagicLiteralBuilder()
+        : state(BuilderState::Initialized),
+          data(new MagicLiteralData())
+    {}
+
+    void MagicLiteralBuilder::loadSMTlib2File(const std::string filename) {
+        snlog::l_warn("Not implemented yet %£1");
+    }
+
+    bool MagicLiteralBuilder::exploitData(DataExploitation e) {
+        snlog::l_warn("Not implemented yet %£2");
+        return false;
+    }
+
+}
