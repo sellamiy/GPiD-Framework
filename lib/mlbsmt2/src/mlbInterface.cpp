8e86e61950fdd50a795bd61993bc1f421f03c347
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2018-09-28 13:19:18 +0200

    Prewrite smtlib2utils version of the mlb

--- lib/mlbsmt2/src/mlbInterface.cpp
+++ lib/mlbsmt2/src/mlbInterface.cpp
@@ -1,19 +1,34 @@
 #define MAGIC_LITERAL_BUILDER_f_SMTLIB2__INTERFACE_CPP
 
 #include <snlog/snlog.hpp>
+#include <smtlib2utils/smtlib2utils.hpp>
 #include <mlbsmt2/mlbconfig.hpp>
 #include <mlbsmt2/mlbInterface.hpp>
 
 namespace mlbsmt2 {
 
-    class MagicLiteralData {
+    class MagicParsingHandler : public smtlib2utils::SMTl2CommandHandler {
 
+        // bool handleSize(const smtlib2utils::SMTl2Command& cmd);
     public:
+        MagicParsingHandler();
+    };
+
+    class MagicLiteralData {
+        MagicParsingHandler handler;
+        smtlib2utils::SMTl2StringMemory smem;
+    public:
+        inline MagicParsingHandler& getHandler() { return handler; }
+        inline smtlib2utils::SMTl2StringMemory& getMemory() { return smem; }
     };
 
 }
 
 using namespace mlbsmt2;
+
+MagicParsingHandler::MagicParsingHandler() {
+    // handlers["<*>"] = std::bind(&MagicParsingHandler::handle<*>, this, std::placeholders::_1);
+};
 
 /*>---------------------------------------<*/
 
@@ -27,7 +42,9 @@ MagicLiteralBuilder::~MagicLiteralBuilder() {
 }
 
 void MagicLiteralBuilder::loadSMTlib2File(const std::string filename) {
-    snlog::l_warn("Not implemented yet %£1");
+    smtlib2utils::SMTl2CommandParser parser(filename, data->getMemory());
+    parser.initialize();
+    parser.parse(data->getHandler());
 }
 
 bool MagicLiteralBuilder::exploitData(DataExploitation e) {
