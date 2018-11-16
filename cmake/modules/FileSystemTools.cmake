64ce23371c56c0a2d6e0c39208e97580589e9dd2
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2018-11-16 16:30:48 +0100

    Rework cmake file lister as a module

---
+++ cmake/modules/FileSystemTools.cmake
@@ -0,0 +1,14 @@
+# ==============================================================================
+# File System Cmake tools
+# ==============================================================================
+macro(subdirlist result curdir)
+  file(GLOB children RELATIVE ${curdir} ${curdir}/*)
+  set(dirlist "")
+  foreach(child ${children})
+    if(IS_DIRECTORY ${curdir}/${child})
+      list(APPEND dirlist ${child})
+    endif()
+  endforeach()
+  set(${result} ${dirlist})
+endmacro()
+# ==============================================================================
