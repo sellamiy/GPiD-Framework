e676d3f1b0f1354fb4832294fd152353f409a72f
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2019-02-08 11:42:16 +0100

    Add string utilities (future)

---
+++ lib/stdutils/stdutils/strings.hpp
@@ -0,0 +1,40 @@
+/**
+ * \file stdutils/strings.hpp
+ * \brief Misc. utils for standard library's strings
+ * \author Yanis Sellami
+ * \date 2019
+ */
+#ifndef LIB_STANDARD_UTILS__STRINGS_HPP
+#define LIB_STANDARD_UTILS__STRINGS_HPP
+
+#include <string>
+#include <vector>
+
+namespace stdutils {
+
+    static inline const std::vector<std::string> split(const std::string& src, const std::string& delim) {
+        size_t _0P = 0;
+        size_t _1P = 0;
+        std::string tk;
+        std::vector<std::string> res;
+        while ((_1P = src.find(delim, _0P)) != std::string::npos) {
+            tk = src.substr(_0P, _1P - _0P);
+            res.push_back(tk);
+            _0P = _1P += delim.length();
+        }
+        return res;
+    }
+
+    static inline size_t count(const std::string& src, const std::string& tgt) {
+        size_t occ = 0;
+        size_t _0P = 0;
+        while ((_0P = src.find(tgt, _0P)) != std::string::npos) {
+            ++occ;
+            _0P += tgt.length();
+        }
+        return occ;
+    }
+
+}
+
+#endif
