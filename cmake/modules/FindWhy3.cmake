816621109067382880780dda4e58e75e4b13a107
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2019-03-04 14:26:24 +0100

    Standard "skip if solver unfound" structure fix-rework

--- cmake/modules/FindWhy3.cmake
+++ cmake/modules/FindWhy3.cmake
@@ -8,3 +8,20 @@ find_program(
 )
 
 mark_as_advanced(WHY3_EXECUTABLE)
+
+# Version
+if (WHY3_EXECUTABLE)
+  execute_process (COMMAND ${WHY3_EXECUTABLE} --version
+    OUTPUT_VARIABLE WHY3_VERSION_STRING
+    ERROR_QUIET
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  string(REPLACE "Why3 platform, version " "" WHY3_VERSION "${WHY3_VERSION_STRING}")
+endif()
+mark_as_advanced(WHY3_VERSION)
+
+# Config
+find_package_handle_standard_args(
+  Why3
+  REQUIRED_VARS WHY3_EXECUTABLE
+  VERSION_VAR WHY3_VERSION
+)
