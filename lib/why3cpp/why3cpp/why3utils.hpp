1b0b2462b5eb14a4e28dbfaf1234415bb42f7179
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2019-03-26 14:51:53 +0100

    Wrap smt2 to why3 conversion data to allow deeper adaptation

--- lib/why3cpp/why3cpp/why3utils.hpp
+++ lib/why3cpp/why3cpp/why3utils.hpp
@@ -6,26 +6,41 @@
 
 namespace why3cpp {
 
-    class Why3Smtl2CV : public lisptp::LispTreeVisitor<std::string> {
+    class Why3ConvertMap {
         const std::set<std::string>& refs;
+    public:
+        Why3ConvertMap();
+        Why3ConvertMap(const std::set<std::string>& refs) : refs(refs) {}
+        Why3ConvertMap(const Why3ConvertMap& o) : refs(o.refs) {}
+
+        inline bool isref(const std::string& t) const { return refs.count(t) > 0; }
+    };
+
+    class Why3Smtl2CV : public lisptp::LispTreeVisitor<std::string> {
+        const Why3ConvertMap& cmap;
     protected:
         inline virtual std::string handle_term(const std::string& t) const override {
-            return (refs.count(t) > 0 ? "!" : "") + t;
+            return (cmap.isref(t) ? "!" : "") + t;
         }
 
         virtual std::string handle_call(const std::string& op, const std::vector<std::string>& lvs)
             const override;
     public:
-        Why3Smtl2CV(const std::set<std::string>& refs) : refs(refs) {}
+        Why3Smtl2CV(const Why3ConvertMap& cmap) : cmap(cmap) {}
 
         inline std::string convert(const std::string& smtl2data) const {
             return visit(lisptp::parse(smtl2data));
         }
     };
 
-    static inline std::string Smt2Why3(const std::string& smtl2data, const std::set<std::string>& refs) {
-        Why3Smtl2CV Smt2Why3Converter(refs);
+    static inline std::string Smt2Why3(const std::string& smtl2data, const Why3ConvertMap& cmap) {
+        Why3Smtl2CV Smt2Why3Converter(cmap);
         return Smt2Why3Converter.convert(smtl2data);
+    }
+
+    static inline std::string Smt2Why3(const std::string& smtl2data, const std::set<std::string>& refs) {
+        Why3ConvertMap cmap(refs);
+        return Smt2Why3(smtl2data, cmap);
     }
 
 }
