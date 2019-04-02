a5e2660ac0968bc32ea48634eaf5d5dca9b9d986
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2019-04-02 14:38:52 +0200

    Insert the mapping storage in cmap containers

--- lib/why3cpp/why3cpp/why3utils.hpp
+++ lib/why3cpp/why3cpp/why3utils.hpp
@@ -2,22 +2,50 @@
 #define LIB_WHY3CPP__PLATFORM_GENERAL_UTILS_HEADER
 
 #include <set>
+#include <stdutils/pairstorage.hpp>
 #include <lisptp/lisptp.hpp>
 
 namespace why3cpp {
 
     class Why3ConvertMap {
         std::set<std::string> refs;
+        stdutils::pair_storage<std::string, std::string> smap_table;
     public:
         Why3ConvertMap() {}
         Why3ConvertMap(const std::set<std::string>& refs) : refs(refs) {}
-        Why3ConvertMap(const Why3ConvertMap& o) : refs(o.refs) {}
+        Why3ConvertMap(const stdutils::pair_storage<std::string, std::string>& smap_table)
+            : smap_table(smap_table) {}
+        Why3ConvertMap(const std::set<std::string>& refs,
+                       const stdutils::pair_storage<std::string, std::string>& smap_table)
+            : refs(refs), smap_table(smap_table) {}
+        Why3ConvertMap(const stdutils::pair_storage<std::string, std::string>& smap_table,
+                       const std::set<std::string>& refs)
+            : refs(refs), smap_table(smap_table) {}
+        Why3ConvertMap(const Why3ConvertMap& o) : refs(o.refs), smap_table(o.smap_table) {}
 
         inline void addRefs(const std::set<std::string>& _refs) {
             for (auto& r : _refs) refs.insert(r);
         }
 
         inline bool isref(const std::string& t) const { return refs.count(t) > 0; }
+
+        inline void addSymbolMapping(const std::string& fwd, const std::string bck) {
+            smap_table.insert(fwd, bck);
+        }
+
+        inline bool hasForwardMapping(const std::string& s) const {
+            return smap_table.contains(s);
+        }
+        inline bool hasBackwardMapping(const std::string& s) const {
+            return smap_table.rcontains(s);
+        }
+
+        inline const std::string& forwardMapping(const std::string& s) const {
+            return smap_table.at(s);
+        }
+        inline const std::string& backwardMapping(const std::string& s) const {
+            return smap_table.rat(s);
+        }
     };
 
     class Why3Smtl2CV : public lisptp::LispTreeVisitor<std::string> {
