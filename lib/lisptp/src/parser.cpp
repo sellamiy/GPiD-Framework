88ae25e524e987478be5a90b4f07d8f5c2507e8a
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2019-04-03 19:06:23 +0200

    Add comment escaping in lisp parser

--- lib/lisptp/src/parser.cpp
+++ lib/lisptp/src/parser.cpp
@@ -7,42 +7,54 @@
 using namespace lisptp;
 using LispTreeNodePtr = std::shared_ptr<LispTreeNode>;
 
-static inline constexpr bool isWSC(char c) {
+static inline constexpr bool isWSC(const char& c) {
     return c == ' ' || c == '\n' || c == '\r' || c == '\t' ;
 }
 
-static inline constexpr bool isOpar(char c) { return c == '('; }
-static inline constexpr bool isCpar(char c) { return c == ')'; }
-static inline constexpr bool isPar(char c) { return isOpar(c) || isCpar(c); }
+static inline constexpr bool isOpar(const char& c) { return c == '('; }
+static inline constexpr bool isCpar(const char& c) { return c == ')'; }
+static inline constexpr bool isPar(const char& c) { return isOpar(c) || isCpar(c); }
 
-static inline constexpr bool isCommentStart(char c) { return c == ';'; }
+static inline constexpr bool isCommentStart(const char& c, const char& _hook) {
+    return _hook == ' ' && c == ';';
+}
+
+static inline constexpr bool isCommentContainer(const char& c, const char& _hook) {
+    return _hook == ' ' ? (c == '\'' || c == '"') : c == _hook;
+}
 
 /*
-static inline void nextWSC(const std::string& data, size_t& pos) {
-    while (pos < data.length() && !isWSC(data.at(pos))) ++pos;
-}
+  static inline void nextWSC(const std::string& data, size_t& pos) {
+  while (pos < data.length() && !isWSC(data.at(pos))) ++pos;
+  }
 */
+
+static inline void updateHook(char& _hook, const char& c) {
+    isCommentContainer(c, _hook) ? _hook = (_hook == ' ' ? c : ' ') : ' ';
+}
 
 static inline void skipComment(const std::string& data, size_t& pos) {
     while (pos < data.length() && data.at(pos) != '\n') ++pos;
 }
 
-static inline void nextHookC(const std::string& data, size_t& pos) {
+static inline void nextHookC(const std::string& data, size_t& pos, char& _hook) {
     while (pos < data.length() && !isWSC(data.at(pos)) && !isPar(data.at(pos))) {
-        if (isCommentStart(data.at(pos))) skipComment(data, pos);
+        updateHook(_hook, data.at(pos));
+        if (isCommentStart(data.at(pos), _hook)) skipComment(data, pos);
         ++pos;
     }
 }
 
-static inline void nextNonWSC(const std::string& data, size_t& pos) {
+static inline void nextNonWSC(const std::string& data, size_t& pos, char& _hook) {
     while (pos < data.length() && isWSC(data.at(pos))) {
-        if (isCommentStart(data.at(pos))) skipComment(data, pos);
+        updateHook(_hook, data.at(pos));
+        if (isCommentStart(data.at(pos), _hook)) skipComment(data, pos);
         ++pos;
     }
 }
 
-static LispTreeNodePtr parse_lisp_text(const std::string& data, size_t& pos) {
-    nextNonWSC(data, pos);
+static LispTreeNodePtr parse_lisp_text(const std::string& data, size_t& pos, char& _hook) {
+    nextNonWSC(data, pos, _hook);
     if (pos >= data.length() || isCpar(data.at(pos))) {
         // TODO: Error
     }
@@ -52,20 +64,20 @@ static LispTreeNodePtr parse_lisp_text(const std::string& data, size_t& pos) {
     if (isOpar(data.at(pos))) {
         callstate = true;
         ++pos;
-        nextNonWSC(data, pos);
+        nextNonWSC(data, pos, _hook);
         size_t loc = pos;
-        nextHookC(data, pos);
+        nextHookC(data, pos, _hook);
         value = data.substr(loc, pos-loc);
-        nextNonWSC(data, pos); // TODO : Possible error case Here
+        nextNonWSC(data, pos, _hook); // TODO : Possible error case Here
         while (!isCpar(data.at(pos))) {
-            leaves.push_back(parse_lisp_text(data, pos));
-            nextNonWSC(data, pos);
+            leaves.push_back(parse_lisp_text(data, pos, _hook));
+            nextNonWSC(data, pos, _hook);
         }
         ++pos;
     } else {
         callstate = false;
         size_t loc = pos;
-        nextHookC(data, pos);
+        nextHookC(data, pos, _hook);
         value = data.substr(loc, pos-loc);
     }
     return LispTreeNodePtr(new LispTreeNode(value, callstate, leaves));
@@ -73,7 +85,8 @@ static LispTreeNodePtr parse_lisp_text(const std::string& data, size_t& pos) {
 
 extern LispTreeNodePtr lisptp::parse(const std::string& data) {
     size_t pos = 0;
-    LispTreeNodePtr global = parse_lisp_text("(" + global_name_wrapper + " " + data + ")", pos);
+    char _hook = ' ';
+    LispTreeNodePtr global = parse_lisp_text("(" + global_name_wrapper + " " + data + ")", pos, _hook);
     if (global->getLeaves().size() == 1)
         return global->getLeaves().front();
     return LispTreeNodePtr(new LispTreeNode("", true, global->getLeaves()));
