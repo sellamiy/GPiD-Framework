fc4ef679a34d6075d9fa666f3dd9a131a6ebed24
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2019-04-29 09:28:01 +0200

    Try float operators autodetection

--- lib/why3cpp/src/why3smtl2c.cpp
+++ lib/why3cpp/src/why3smtl2c.cpp
@@ -26,6 +26,16 @@ static inline bool is_prefix(const std::string& s) {
     return s == "-";
 }
 
+static inline const std::string floatize(const std::string& s) {
+    return (s == "=" || s == "<>") ? s : s + ".";
+}
+
+static inline bool detected_float(const std::string& data) {
+    return data.find("from_int") != std::string::npos
+        || data.find(".") != std::string::npos;
+    // TODO: Add correct handle for variables
+}
+
 std::string Why3Smtl2CV::handle_call(const std::string& op, const std::vector<std::string>& lvs) const {
     const std::string& ops = cmap.hasForwardMapping(op) ? cmap.forwardMapping(op) : op;
     if (ops == "and" || ops == "AND")
@@ -38,8 +48,12 @@ std::string Why3Smtl2CV::handle_call(const std::string& op, const std::vector<st
         return parenthize("not " + stdutils::join("", lvs));
     if (is_prefix(ops))
         return parenthize(ops + " " + lvs.front());
-    if (is_infix(ops))
+    if (is_infix(ops)) {
+        for (const std::string& p : lvs)
+            if (detected_float(p))
+                return parenthize(stdutils::join(space(floatize(ops)), lvs));
         return parenthize(stdutils::join(space(ops), lvs));
+    }
     return parenthize(ops + " " + stdutils::join(" ", lvs));
 }
 
