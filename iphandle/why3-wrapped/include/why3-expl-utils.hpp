37cd9bc3efefea70f929160e53458ca91fa1db01
Yanis Sellami <yanis.sellami@univ-grenoble-alpes.fr>
2019-05-06 16:43:18 +0200

    Update strengthenable expls

--- iphandle/why3-wrapped/include/why3-expl-utils.hpp
+++ iphandle/why3-wrapped/include/why3-expl-utils.hpp
@@ -4,10 +4,12 @@
 #include <string>
 #include <snlog/snlog.hpp>
 
-static inline bool isStrengthenableExplanation(const std::string& expl, bool forwardEmpty=false) {
+static inline bool isStrengthenableExplanation
+(const std::string& expl, bool forwardEmpty=false, bool forwardInit=false) {
     return expl == "expl:postcondition"
         || expl == "expl:exceptional postcondition"
         || expl == "expl:assertion"
+        || expl == "expl:precondition"
         || expl == "expl:check"
         || expl == "expl:lemma"
         || expl == "expl:unreachable point" // TODO: Check relevancy
@@ -18,12 +20,21 @@ static inline bool isStrengthenableExplanation(const std::string& expl, bool for
         || expl == "expl:variant decrease" // TODO: Check relevancy
         || expl == "expl:type invariant" // TODO: Check relevancy
         || expl == "expl:termination" // TODO: Check relevancy
+        || expl == "expl:check modulo by zero"
+        || expl == "expl:index in array bounds"
+        || expl == "expl:array creation size"
         || (forwardEmpty && expl == "")
+        || (forwardInit && expl == "expl:loop invariant init")
+        || expl.find("expl:VC for ") == 0
         ;
 }
 
 static inline bool isForwardingExplanation(const std::string& expl) {
     return expl == "expl:loop invariant preservation";
+}
+
+static inline bool isInitExplanation(const std::string& expl) {
+    return expl == "expl:loop invariant init";
 }
 
 #endif
